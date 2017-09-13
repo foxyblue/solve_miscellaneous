aa7343600dda4c4ae9365496ddc376c870ab602c
Sebastien (Work) <sebastien@cytora.com>
2017-09-13 15:13:50 +0100

    Add test grid object
    Add script containing an object to trial map_objects.

--- mapping/test_grid.py
+++ mapping/test_grid.py
@@ -1,21 +1,23 @@
 
+class Tile:
+    def __init__(sekf)
 
-
-class Grid:
-    def __init__(self, grid_h, grid_v, add_areas=0, area_type=None):
-        self.grid_h = grid_h
-        self.grid_v = grid_v
-        self.add_areas = add_areas
+class Area:
+    """Mapping out an area with a horizontal and vertical value."""
+    def __init__(self, h, v, area_type=None):
+        self.h = h
+        self.v = v
         self.area_type = area_type
         
-        self.create_grid()
+        self.create_area()
         
-    def create_grid(self):
+    def create_area(self):
         self.grid = [[
             (x, y)
-            for x in range(self.grid_h)]
-                for y in range(self.grid_v)        
+            for x in range(self.h)]
+                for y in range(self.v)        
         ]
 
         
-print Grid(10,10).grid
+for n in Area(10,10).grid:
+    print n
