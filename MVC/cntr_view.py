7b90ff1e2e9be1a57a652ac17f0d2b156a704ef8
GiantsLoveDeathMetal <sebastien@cytora.com>
2018-05-03 00:35:47 +0100

    Iterate on MVC + S

--- MVC/cntr_view.py
+++ MVC/cntr_view.py
@@ -1,30 +1,6 @@
 # from note_mvc import TagsModel
 from note_mvc import NotesModel
-
-
-class Controller:
-
-    def __init__(self, model): #, view):
-        self.model = model
-        # self.view = view
-
-    def basic_output(self):
-        menu_items = self.model.items()
-        return self.model.sort_items()
-
-    def query_output(self, query):
-        menu_items = self.model.items()
-        query_result = self.model.query(menu_items, query)
-        return self.model.sort_items()
-
-    def sub_query_output(self, subquery):
-        menu_items = self.model.items()
-        query_result = self.model.sub_query(menu_items, subquery)
-        return self.model.sort_items()
-
-
-
-
+from note_mvc import Service
 
 #    tui_object = {
 #        "title": string,
@@ -32,18 +8,59 @@ class Controller:
 #        "subheadings": list[string, string],
 #    }
 
-# class View:
-#     def __init__(self):
-#         pass
-#     def attach_info(self, items, info):
-#         for item in items:
 
+# Show this, hide that etc
+# forwards info to view
+# handles dictation of models
+
+class Controller:
+
+    def __init__(self, model):
+        self.model = model
+        self.service = Service(model)
+        self.menu_items = self.model.items()
+
+    def basic_output(self):
+        return self.model.sort_items()
+
+    def query_output(self, query):
+        query_result = self.model.query(self.menu_items, query)
+        return self.model.sort_items()
+
+    def sub_query_output(self, subquery):
+        query_result = self.model.sub_query(self.menu_items, subquery)
+        return self.model.sort_items()
+
+""" Prototype View Interface
+def list_notes(query):
+    notes_model = NotesModel()
+    control = Controller(notes_model)
+    if query:
+        return display_list(control.query_output(query))
+    else:
+        return display_list(note_control.basic_output())
+def list_tags(query):
+    tags_model = TagsModel()
+    control = Controller(notes_model)
+    if query:
+        return display_list(control.query_output(query))
+    else:
+        return display_list(control.basic_output())
+def list_search(query):
+    notes_model = NotesModel()
+    control = Controller(notes_model)
+    if query:
+        return display_list(control.model_query(query))
+    else:
+        raise NotImplementedError
+"""
 
 
 notes_model = NotesModel()
 # view = View()
 
 note_control = Controller(notes_model) #, view)
+
 
 for x in note_control.basic_output():
     print(x)
--- MVC/note_mvc.py
+++ MVC/note_mvc.py
@@ -22,8 +22,14 @@ class Model:
         raise NotImplementedError
 
     @property
-    def item_type(self):
+    def model_type(self):
         raise NotImplementedError
+
+    def items(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        return len(self.items)
 
 # 1.)
 # The order is part of the model, so it should go there.
@@ -37,6 +43,7 @@ def tui_object(x, y, z):
 
 class NotesModel(Model):
     notes = load_meta()
+    model_type = 'notes'
 
     def get(self, title):
         try:
@@ -56,13 +63,30 @@ class NotesModel(Model):
     def items(self):
         return self.notes.keys()
 
-    def sort_items(self):
+    def get_views(self, title):
+        return self.get(title)['views']
+
+
+class Service:
+
+    SORT_RULE = 5
+    TOP_n_VIEWED = 3
+
+    def __init__(self, model):
+        self.model = model
+        self.model_length = len(model)
+
+    def sort(self):
+        if self.model_length > SORT_RULE:
+            return clever_sort()
+        return alphabetize()
+
+    def alphabetize(self):
         iterable = self.notes
         sort = sorted(iterable, key=lambda x: x[0].lower())
         return tui_object(sort, self.get_info(sort), self.get_subinfo(sort))
 
-    def get_views(self, title):
-        return self.get(title)['views']
+
 
 
 if __name__ == '__main__':
